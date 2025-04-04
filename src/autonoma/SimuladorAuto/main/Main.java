43e43fa75347b8f195410e0e9708f4280b73278a
Gilary Rugeles Avendaño <gilaryv.rugelesa@autonoma.edu.co>
2025-04-04 17:48:50 -0500

    Se realizo la ventana principal, el JDialog CarroEncendido, se completo la clase main y la clase ConfiguracionDelVehiculo

--- src/autonoma/SimuladorAuto/main/Main.java
+++ src/autonoma/SimuladorAuto/main/Main.java
@@ -1,4 +1,11 @@
 package autonoma.SimuladorAuto.main;
+
+import autonoma.SimuladorAuto.models.ConfiguracionDelVehiculo;
+import autonoma.SimuladorAuto.models.Simulador;
+import autonoma.SimuladorAuto.models.Vehiculo;
+import autonoma.SimuladorAuto.views.Principal;
+import java.io.IOException;
+
 /**
  * @author Gilary
  * @since 20250328
@@ -7,8 +14,10 @@ package autonoma.SimuladorAuto.main;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+
+        Vehiculo vehiculo = ConfiguracionDelVehiculo.cargarVehiculo("src/autonoma/SimuladorAuto/txt/Configuracion.txt");
+        Simulador simulador = new Simulador(vehiculo);
+        new Principal(simulador).setVisible(true);
     }
-    
 }
--- src/autonoma/SimuladorAuto/models/ConfiguracionDelVehiculo.java
+++ src/autonoma/SimuladorAuto/models/ConfiguracionDelVehiculo.java
@@ -1,13 +1,62 @@
 
 package autonoma.SimuladorAuto.models;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @author Gilary
  * @since 20250328
  * @version 1.0
  */
 public class ConfiguracionDelVehiculo {
+        
+    public static Vehiculo cargarVehiculo(String archivoConfiguracion) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(archivoConfiguracion));
+        String linea;
+        Llanta llantas = null;
+        Motor motor = null;
+        while ((linea = br.readLine()) != null) {
+            String[] partes = linea.split(";");
+            switch (partes[0]) {
+                case "llantas":
+                    llantas = crearLlantas(partes[1]);
+                    break;
+                case "motor":
+                    motor = crearMotor(Integer.parseInt(partes[1]));
+                    break;
+            }
+        }
+        br.close();
+        return new Vehiculo(motor, llantas);
+    }
     
+    private static Llanta crearLlantas(String tipo) {
+        switch (tipo) {
+            case "Buenas":
+                return new Llanta("Buenas", 110); // Límite de velocidad para "Buenas"
+            case "Bonitas":
+                return new Llanta("Bonitas", 70); // Límite de velocidad para "Bonitas"
+            case "Baratas":
+                return new Llanta("Baratas", 50); // Límite de velocidad para "Baratas"
+            default:
+                throw new IllegalArgumentException("Tipo de llanta no válido: " + tipo);
+        }
+    }
+
+    private static Motor crearMotor(int cilindraje) {
+        switch (cilindraje) {
+            case 1000:
+                return new Motor(100); // Velocidad máxima para 1000 cc
+            case 2000:
+                return new Motor(160); // Velocidad máxima para 2000 cc
+            case 3000:
+                return new Motor(220); // Velocidad máxima para 3000 cc
+            default:
+                throw new IllegalArgumentException("Cilindraje no válido: " + cilindraje);
+        }
+    }
     
 }
 
