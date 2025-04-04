ad4a6b66ea70e26d90a381e392ff497d7369796d
Gilary Rugeles Avendaño <gilaryv.rugelesa@autonoma.edu.co>
2025-04-04 10:50:32 -0500

    Se agrego el archivo de NetBeans y el .gitignore

---
+++ src/autonoma/SimuladorAuto/exception/AccidenteException.java
@@ -0,0 +1,19 @@
+
+package autonoma.SimuladorAuto.exception;
+
+/**
+ Exception lanzada cuando el vehiculo se accidenta, ya sea por apagarlo
+ a alta velocidad o por acelerar mas alla de su limite
+ * @author Gilary
+ * @since 20250328
+ * @version 1.0
+ */
+public class AccidenteException extends Exception {
+
+    /**
+     * Constructor de la excepcion
+     */
+    public AccidenteException(String mensaje ){
+        super(mensaje);
+    }
+}
